e06959157d37d4d5ca6330a5dd07e7535b83fa6a
sstae <sutham.tae@gmail.com>
2015-09-17 10:25:43 +0700

    update GFX.core

---
+++ ContractUs.DataAccess/TicketRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GFX.Core;
+using ContactUs.Models;
+
+namespace ContractUs.DataAccess
+{
+    public class TicketRepository : RepositoryBase<Ticket>{
+
+    }
+}
